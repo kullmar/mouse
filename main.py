4dd495a92b5133642469721bc700a7e34f815ef0
Markus Kullberg <kullberg.markus@gmail.com>
2020-08-14 23:32:25 +0300

    WIP

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pynput.mouse import Listener, Button
+from pynput.mouse import Listener, Button, Controller
 from enum import Enum
 import time
 import csv
@@ -17,7 +17,7 @@ class MouseEvent(Enum):
     MIDDLE_MOUSE_RELEASED = 7
 
     @staticmethod
-    def of_button(button, pressed):
+    def from_button(button, pressed):
         if pressed:
             if button == Button.right:
                 return MouseEvent.RIGHT_MOUSE_PRESSED
@@ -45,7 +45,7 @@ class MouseRecorder:
         self.write(MouseEvent.MOVED, x, y)
 
     def on_click(self, x, y, button, pressed):
-        self.write(MouseEvent.of_button(button, pressed), x, y)
+        self.write(MouseEvent.from_button(button, pressed), x, y)
 
     def write(self, event, x, y):
         self.csv_file.writerow([event.name, x, y, time.perf_counter() - self.start_time])
@@ -77,31 +77,44 @@ class ClickAnalyzer:
         with open(self.file_path, newline='') as csvfile:
             csv_reader = csv.reader(csvfile)
             next(csv_reader)
-            d = analyze_mouse_pressed_duration(csv_reader)
-            print(d)
+            d = analyze_mouse_press_duration(csv_reader)
+            print('Mouse press duration: ', d)
             csvfile.seek(0)
             next(csv_reader)
             c = analyze_time_between_clicks(csv_reader)
-            print(c)
+            print('Time between clicks: ', c)
 
 
-def analyze_mouse_pressed_duration(csv_reader):
+def analyze_mouse_press_duration(csv_reader):
+    durations = extract_mouse_press_durations(csv_reader)
+
+    return statistics.NormalDist.from_samples(durations)
+
+
+def extract_mouse_press_durations(csv_reader):
     durations_ms = []
-    is_pressed = False
     left_mouse_down_time = None
+    row = find_next_row_with_event(csv_reader, MouseEvent.LEFT_MOUSE_PRESSED)
 
-    for row in csv_reader:
-        print(row)
-        event_of_interest = MouseEvent.LEFT_MOUSE_RELEASED.name if is_pressed else MouseEvent.LEFT_MOUSE_PRESSED.name
+    while row is not None:
         current_event = row[0]
-        if current_event == event_of_interest:
-            if current_event == MouseEvent.LEFT_MOUSE_RELEASED.name:
-                durations_ms.append(float(row[3]) - left_mouse_down_time)
-            else:
-                left_mouse_down_time = float(row[3])
-            is_pressed = current_event == MouseEvent.LEFT_MOUSE_PRESSED.name
+        if current_event == MouseEvent.LEFT_MOUSE_RELEASED.name:
+            durations_ms.append(float(row[3]) - left_mouse_down_time)
+        else:
+            left_mouse_down_time = float(row[3])
+        is_pressed = current_event == MouseEvent.LEFT_MOUSE_PRESSED.name
+        next_event_of_interest = MouseEvent.LEFT_MOUSE_RELEASED if is_pressed else MouseEvent.LEFT_MOUSE_PRESSED
+        row = find_next_row_with_event(csv_reader, next_event_of_interest)
 
-    return statistics.NormalDist.from_samples(durations_ms)
+    return durations_ms
+
+
+def find_next_row_with_event(csv_reader, event):
+    for row in csv_reader:
+        if row[0] == event.name:
+            return row
+
+    return None
 
 
 def analyze_time_between_clicks(csv_reader):
@@ -115,6 +128,7 @@ def analyze_time_between_clicks(csv_reader):
                 times.append(tm - previous)
             previous = tm
 
+
     return statistics.NormalDist.from_samples(times)
 
 
@@ -127,10 +141,10 @@ class Application(tk.Frame):
         self.mouse_recorder = MouseRecorder()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Start recording"
-        self.hi_there["command"] = self.start_recording
-        self.hi_there.pack(side="top")
+        self.record_button = tk.Button(self)
+        self.record_button["text"] = "Start recording"
+        self.record_button["command"] = self.start_recording
+        self.record_button.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
@@ -139,21 +153,36 @@ class Application(tk.Frame):
     def start_recording(self):
         self.recording_thread = threading.Thread(target=self.mouse_recorder.start)
         self.recording_thread.start()
-        self.hi_there["text"] = "Stop recording"
-        self.hi_there["command"] = self.stop_recording
+        self.record_button["text"] = "Stop recording"
+        self.record_button["command"] = self.stop_recording
 
     def stop_recording(self):
         self.mouse_recorder.stop()
         self.recording_thread.join()
-        self.hi_there["text"] = "Start recording"
-        self.hi_there["command"] = self.start_recording
+        self.record_button["text"] = "Start recording"
+        self.record_button["command"] = self.start_recording
+
+
+class AutoClicker:
+    def __init__(self, mouse_press_dist, time_between_click_dist):
+        self.mouse_press_dist = mouse_press_dist
+        self.time_between_click_dist = time_between_click_dist
+        self.is_running = False
+
+    def start(self):
+        mouse = Controller()
+
+        self.is_running = True
+        while self.is_running:
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    root = tk.Tk()
-    app = Application(master=root)
-    app.mainloop()
-    # analyzer = ClickAnalyzer('mouse-recording.csv')
-    # analyzer.analyze()
+    # root = tk.Tk()
+    # app = Application(master=root)
+    # app.mainloop()
+    analyzer = ClickAnalyzer('mouse-recording.csv')
+    analyzer.analyze()
     # recorder = MouseRecorder()
     # recorder.start()
