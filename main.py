e6663df62be841383cece356d1e8176b4ecfdf30
Markus Kullberg <kullberg.markus@gmail.com>
2020-08-19 23:29:20 +0300

    WIP

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from pynput.mouse import Listener, Button, Controller
-from enum import Enum
-import time
 import csv
-import tkinter as tk
 import threading
+import time
+import tkinter as tk
+import ctypes
+import logging
+from abc import ABC, abstractmethod
+from enum import Enum
+
+from pynput.mouse import Listener, Button, Controller
 from scipy import stats
 
 
@@ -121,9 +125,9 @@ def analyze_time_between_clicks(csv_reader):
         previous_mouse_press_time = tm
         row = find_next_row_with_event(csv_reader, MouseEvent.LEFT_MOUSE_PRESSED)
 
-    even_click_times = times[::2]
-    odd_click_times = times[1::2]
-    return stats.gamma.fit(even_click_times), stats.gamma.fit(odd_click_times)
+    item_click_times = times[::2]
+    spell_click_times = times[1::2]
+    return stats.gamma.fit(item_click_times), stats.gamma.fit(spell_click_times)
 
 
 def find_next_row_with_event(csv_reader, event):
@@ -143,7 +147,7 @@ class Application(tk.Frame):
         self.mouse_recorder = MouseRecorder()
         analyzer = ClickAnalyzer('mouse_recording.csv')
         analyzer.analyze()
-        self.auto_alcher = AutoAlcher(analyzer.mouse_press_dist, analyzer.time_between_click_dist)
+        self.auto_alcher = AutoAlcher(Mouse(), analyzer.time_between_click_dist)
 
     def create_widgets(self):
         self.record_button = tk.Button(self)
@@ -178,48 +182,149 @@ class Application(tk.Frame):
             self.auto_alcher_thread.join()
             self.auto_alch_button["text"] = "Start auto alcher"
         else:
-            self.auto_alcher_thread = threading.Thread(target=self.auto_alcher.start)
+            self.auto_alcher_thread = threading.Thread(target=self.auto_alcher.run)
             self.auto_alcher_thread.start()
             self.auto_alch_button["text"] = "Stop auto alcher"
 
 
-class AutoAlcher:
-    def __init__(self, mouse_press_dist, time_between_click_dist):
-        self.mouse_press_dist = mouse_press_dist
+class Script(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class AutoAlcher(Script):
+    class State(Enum):
+        CLICK_SPELL = 1
+        CLICK_ITEM = 2
+        IDLE = 3
+        STOPPED = 4
+
+    def __init__(self, mouse, time_between_click_dist):
+        super().__init__("Auto Alcher")
         self.time_between_click_dist = time_between_click_dist
         self.is_running = False
-        self.mouse = Controller()
         self.click_count = 0
+        self.current_state = self.State.STOPPED
+        self.mouse = mouse
+        self.previous_animation_started_at = None
 
-    def start(self):
+    def run(self):
+        logging.info('Starting auto alcher')
         self.is_running = True
         while self.is_running:
-            self.click()
-            self.wait()
+            if not is_runelite_in_foreground():
+                logging.info('RuneLite not in foreground. Sleeping for 5 sec')
+                self.set_state(self.State.STOPPED)
+                time.sleep(5)
+            else:
+                self.click()
+                self.short_idle()
+                self.click()
+                self.long_idle()
 
     def stop(self):
         self.is_running = False
-        print(f'Stopped after {self.click_count} clicks')
+        logging.info(f'Stopped after {self.click_count} clicks')
+        self.click_count = 0
+
+    def click(self):
+        self.mouse.click()
+        self.click_count += 1
+
+    def short_idle(self):
+        a, loc, beta = self.time_between_click_dist[0]
+        sleep_time = stats.gamma.rvs(a, loc=loc, scale=beta)
+        logging.debug(f'Idling for {sleep_time} sec')
+        time.sleep(sleep_time)
+
+    def long_idle(self):
+        a, loc, beta = self.time_between_click_dist[1]
+        sleep_time = stats.gamma.rvs(a, loc=loc, scale=beta)
+        logging.debug(f'Idling for {sleep_time} sec')
+        time.sleep(sleep_time)
+
+    def idle(self):
+        idx = 1 if self.current_state == self.State.CLICK_ITEM else 0
+        self.set_state(self.State.IDLE)
+        a, loc, beta = self.time_between_click_dist[idx]
+        sleep_time = stats.gamma.rvs(a, loc=loc, scale=beta)
+        logging.debug(f'Idling for {sleep_time} sec')
+        time.sleep(sleep_time)
+
+    def set_state(self, state):
+        if state != self.current_state:
+            self.current_state = state
+            logging.debug(f'New state: {state.name}')
+
+
+def focus_window(name):
+    handle = find_window(name)
+    if handle:
+        ctypes.windll.user32.SetForegroundWindow(handle)
+        return True
+    return False
+
+
+def find_window(name):
+    return ctypes.windll.user32.FindWindowW(None, name)
+
+
+def is_runelite_in_foreground():
+    user32 = ctypes.windll.user32
+    h_wnd = user32.GetForegroundWindow()
+    length = user32.GetWindowTextLengthW(h_wnd) + 1
+    title = ctypes.create_unicode_buffer(length)
+    user32.GetWindowTextW(h_wnd, title, length)
+
+    return 'RuneLite' in title.value
+
+
+class Mouse:
+    def __init__(self):
+        self.controller = Controller()
+        analyzer = ClickAnalyzer('mouse_recording.csv')
+        analyzer.analyze()
+        self.mouse_press_dist = analyzer.mouse_press_dist
 
     def click(self):
         a, loc, beta = self.mouse_press_dist
-
         mouse_down_duration = stats.gamma.rvs(a, loc=loc, scale=beta)
-        print(f'Left mouse click with {mouse_down_duration} duration')
-        self.mouse.press(Button.left)
+        self.controller.press(Button.left)
         time.sleep(mouse_down_duration)
-        self.mouse.release(Button.left)
-        self.click_count += 1
+        self.controller.release(Button.left)
 
-    def wait(self):
-        idx = self.click_count % 2
-        a, loc, beta = self.time_between_click_dist[idx]
-        sleep_time = stats.gamma.rvs(a, loc=loc, scale=beta)
-        print(f'Sleeping for {sleep_time} seconds')
-        time.sleep(sleep_time)
+    def move_to(self, x, y):
+        pass
+
+
+class Bot:
+    def __init__(self):
+        self.active_script = None
+        self.runelite_client = None
+        self.mouse = Mouse()
+
+    def start_script(self):
+        if self.active_script:
+            logging.info(f'Starting script: {self.active_script.name}')
+            self.active_script.run()
+
+    def stop_script(self):
+        if self.active_script:
+            logging.info(f'Stopping script: {self.active_script.name}')
+            self.active_script.stop()
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
+    logging.basicConfig(level=logging.DEBUG)
+    root = tk.Tk(className='Kränkbot')
+    root.geometry('300x200')
     app = Application(master=root)
     app.mainloop()
